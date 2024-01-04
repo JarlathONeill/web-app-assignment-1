89a73bf8d5fdf91ee0459ae4c0d996c5409d6631
Jarlath O'Neill <30296432+JarlathONeill@users.noreply.github.com>
2024-01-04 18:09:37 +0000

    restart

--- server/app.js
+++ server/app.js
@@ -1,10 +1,46 @@
 const express = require('express');
 const app = express();
+
 const cors = require('cors');
+
 const dotenv = require('dotenv');
 dotenv.config();
+//const morgan = require('morgan');
+//app.use(morgan('tiny'));
 
 app.use(cors());
+
 app.use(express.json());
-app.use(express.urlencoded({extended : false}));
+
+app.use(express.urlencoded({ extended : false}));
+
+app.use('/js', express.static(__dirname + '/public/js'));
+app.use('/css', express.static(__dirname + '/public/css'));
+
+app.listen(process.env.PORT, () => console.log('app is running'));
+
+
+
+
+
+
+
+/*
+app.get('/', (req, res) => {
+    res.status(200);
+    res.render('index');
+});
+
+app.get('*', (req, res) => {
+    res.status(404);
+    res.send('<h1>Page not found...</h1>');
+});
+
+app.listen(PORT, (err) => {
+    if (err)
+        return console.log(err);
+    console.log(`Express Web Server listening on http://localhost:${PORT}`);
+});
+*/
+
 
